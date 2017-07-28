69537ebc926a3f853fbb22bb38543ef1d4b43e7a
abdulraoof003 <abdulraoof003@yahoo.co.uk>
2017-07-28 15:25:43 +0100

    28/07/2017 3

--- src/BarrenMoore/Game.java
+++ src/BarrenMoore/Game.java
@@ -5,20 +5,24 @@ import java.util.Scanner;
 public class Game {
 
 	static int playerXLoc, playerYLoc = 0;
+	int treasuresCollected = 0;
+	int livesRemaining = 3;
+
 	
+	
+	private char[][] grid;
+
 	public void createMap() {
 
-
-		char grid[][] = new char[12][12];
-
+		grid = new char[12][12];
 
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid.length; j++) {
 				double random = Math.random();
 				if (random <= .05) {
-					grid[i][j] = '*';
+					grid[i][j] = 'L';
 				} else if (random > .06 && random <= .15) {
-					grid[i][j] = 'X';
+					grid[i][j] = 'T';
 				} else {
 					grid[i][j] = '.';
 				}
@@ -30,7 +34,6 @@ public class Game {
 		}
 	}
 
-
 	public void movement() {
 		
 		String move = "";
@@ -38,12 +41,39 @@ public class Game {
 		move = InputClass.sc.nextLine();
 
 		switch (move) {
-		  case "u": playerYLoc += 1; break;
-		  case "d": playerYLoc -= 1; break;
-		  case "r": playerXLoc += 1; break;
-		  case "l": playerXLoc -= 1; break;
+
+		  case "u": playerXLoc -= 1; break;
+		  case "d": playerXLoc += 1; break;
+		  case "r": playerYLoc += 1; break;
+		  case "l": playerYLoc -= 1; break;
+		
+		
+		
+//		  case "u": playerYLoc += 1; break;  //goes right
+//		  case "d": playerYLoc -= 1; break;  //goes left
+//		  case "r": playerXLoc += 1; break;  //goes down
+//		  case "l": playerXLoc -= 1; break;  //goes up
 		}
-		System.out.println("Contents of location x " + playerXLoc); 		
+		if (grid [playerXLoc] [playerYLoc] == 'T') {
+			treasuresCollected += 1;}
+		else if (grid [playerXLoc] [playerYLoc] == 'L') {
+			livesRemaining -= 1;
+		}
+
+		grid[playerXLoc][playerYLoc] = 'P';
+		
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid.length; j++) {
+				System.out.print(grid[i][j]);
+			}
+			System.out.println("");}
+		
+		
+		
+		System.out.println("Treasures Collected " + treasuresCollected);
+		System.out.println("Lives remaining " + livesRemaining);
+		
+ 		
 	}
 	
 }
